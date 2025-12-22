5558773398780b5a12a53235f2612a0b7e66e67a
Mark <3mark.7395g@gmail.com>
2025-12-23 00:49:53 +0100

    - port to 1.21.9-10

--- src/main/java/net/mark/noteblockdecrement/item/ModItem.java
+++ src/main/java/net/mark/noteblockdecrement/item/ModItem.java
@@ -3,36 +3,35 @@ package net.mark.noteblockdecrement.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.mark.noteblockdecrement.NoteblockDecrement;
-import net.minecraft.item.*;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.Rarity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 
 import java.util.function.Function;
 
 public class ModItem {
 
-    public static final Item NOTEBLOCK_DECREMENTER = registerItem("noteblock_decrementer", setting -> new NoteblockDecrementItem(setting.maxCount(1).rarity(Rarity.EPIC)));
+    public static final Item NOTEBLOCK_DECREMENTER = registerItem("noteblock_decrementer", properties -> new NoteblockDecrementItem(properties.stacksTo(1).rarity(Rarity.EPIC)));
 
 
 
 
-    private static Item registerItem(String name, Function<Item.Settings, Item> function) {
-        return Registry.register(Registries.ITEM, Identifier.of(NoteblockDecrement.MOD_ID, name),
-                function.apply(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NoteblockDecrement.MOD_ID, name)))));
+    private static Item registerItem(String name, Function<Item.Properties, Item> function) {
+        return Registry.register(BuiltInRegistries.ITEM, Identifier.fromNamespaceAndPath(NoteblockDecrement.MOD_ID, name),
+                function.apply(new Item.Properties().setId(ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(NoteblockDecrement.MOD_ID, name)))));
     }
 
 
 
 
     public static void registerModItems() {
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.OPERATOR).register(entries -> {
-            entries.add(ModItem.NOTEBLOCK_DECREMENTER);
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.OP_BLOCKS).register(entries -> {
+            entries.accept(ModItem.NOTEBLOCK_DECREMENTER);
         });
     }
 }
