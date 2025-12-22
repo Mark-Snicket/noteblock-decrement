158c201e53e910f3ed1f81ee71d9694df4dbb8da
Mark <3mark.7395g@gmail.com>
2025-12-22 02:53:48 +0100

    initial commit

---
+++ src/main/java/net/mark/noteblockdecrement/NoteblockDecrement.java
@@ -0,0 +1,14 @@
+package net.mark.noteblockdecrement;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NoteblockDecrement implements ModInitializer {
+    public static final String MOD_ID = "noteblock-decrement";
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/mark/noteblockdecrement/NoteblockDecrementClient.java
@@ -0,0 +1,11 @@
+package net.mark.noteblockdecrement;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoteblockDecrementClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
