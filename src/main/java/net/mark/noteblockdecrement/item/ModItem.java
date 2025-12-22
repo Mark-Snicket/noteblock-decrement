89504f6a1c02c25fd30226a297219231cc53a44c
Mark <3mark.7395g@gmail.com>
2025-12-22 23:41:35 +0100

    - port to 1.21.2-8 - added the item json for 1.21.4-8

--- src/main/java/net/mark/noteblockdecrement/item/ModItem.java
+++ src/main/java/net/mark/noteblockdecrement/item/ModItem.java
@@ -6,17 +6,25 @@ import net.mark.noteblockdecrement.NoteblockDecrement;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+
+import java.util.function.Function;
 
 public class ModItem {
 
-    public static final Item NOTEBLOCK_DECREMENTER = registerItem(new NoteblockDecrementItem(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
+    public static final Item NOTEBLOCK_DECREMENTER = registerItem("noteblock_decrementer", setting -> new NoteblockDecrementItem(setting.maxCount(1).rarity(Rarity.EPIC)));
 
 
 
-    private static Item registerItem(Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(NoteblockDecrement.MOD_ID, "noteblock_decrementer"), item);
+
+    private static Item registerItem(String name, Function<Item.Settings, Item> function) {
+        return Registry.register(Registries.ITEM, Identifier.of(NoteblockDecrement.MOD_ID, name),
+                function.apply(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NoteblockDecrement.MOD_ID, name)))));
     }
 
 
